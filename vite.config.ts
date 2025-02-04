82a97d795ce539e6cfc26a61c68ac516e0db6707
Manas Khobrekar <112411630+insert-username-sample@users.noreply.github.com>
2025-02-05 03:34:44 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Add this line
+  base: '/',
+  root: path.resolve(__dirname, '.'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
